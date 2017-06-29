f7945dcbcb8d84b661bac241258f73dd11aae645
Richard Saville <richardasaville@gmail.com>
2017-06-29 14:55:00 +1000

    calculate stocks percentage change and SP500 percentage change

--- Parsing.py
+++ Parsing.py
@@ -9,13 +9,29 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
     statspath = path+'/_KeyStats'
     stock_list = [x[0] for x in os.walk(statspath)]
     #insert data frame
-    df = pd.DataFrame(columns = ['Date','Unix','Ticker','DE Ratio','Price'])
+    df = pd.DataFrame(columns = ['Date',
+                                 'Unix',
+                                 'Ticker',
+                                 'DE Ratio',
+                                 'Price',
+                                 'stock_p_change',
+                                 'SP500',
+                                 'sp500_p_change'])
     #print(stock_list)
+    #get SP500 data from csv file.
     sp500_df = pd.DataFrame.from_csv("SP500.csv")
+
+    ticker_list = []
 
     for each_dir in stock_list[1:2]:
         each_file = os.listdir(each_dir)
         ticker = each_dir.split("_KeyStats/")[1]
+        ticker_list.append(ticker)
+
+        #calculate %change, start over each time stock changes.
+        starting_stock_value = False
+        starting_sp500_value = False
+
         if len(each_file) > 0:
             for file in each_file:
 
@@ -25,30 +41,42 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
                 full_file_path = each_dir+'/'+file
                 #save full source code HTML to "source" variable.
                 source = open(full_file_path,'r').read()
-                #print(source)
 
                 try:
                     #split by the opening of the table data tag and the table data closing tag to find the value we're hunting for.
                     value = source.split(gather+':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0]
 
-                    # try:
-                    #     sp500_date = datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d')
-                    #     row = sp500_df[(sp500_df.index == sp500_date)]
-                    #     sp500_value = float(row["Adjusted Close"])
-                    # except:
-                    #     sp500_date = datetime.fromtimestamp(unix_time-259200).strftime('%Y-%m-%d')
-                    #     row = sp500_df[(sp500_df.index == sp500_date)]
-                    #     sp500_value = float(row["Adjusted Close"])
+                    try:
+                        sp500_date = datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d')
+                        row = sp500_df[(sp500_df.index == sp500_date)]
+                        sp500_value = float(row["Adj Close"])
+                    except:
+                        sp500_date = datetime.fromtimestamp(unix_time-259200).strftime('%Y-%m-%d')
+                        row = sp500_df[(sp500_df.index == sp500_date)]
+                        sp500_value = float(row["Adj Close"])
 
                     stock_price = float(source.split('</small><big><b>')[1].split('</b></big>')[0])
                     #print("stock_price:",stock_price,"ticker:", ticker)
+                    
+                    #if false, set starting value
+                    if not starting_stock_value:
+                        starting_stock_value = stock_price
+                    if not starting_sp500_value:
+                        starting_sp500_value = sp500_value
+
+                    #calculate % change (new-old)/old * 100
+                    stock_p_change = ((stock_price - starting_stock_value) / starting_stock_value) * 100
+                    sp500_p_change = ((sp500_value - starting_sp500_value) / starting_sp500_value) * 100
 
                     #re-defining our DataFrame object as the previous DataFrame object with the new data appended to it
                     df = df.append({'Date':date_stamp,
                                     'Unix':unix_time,
                                     'Ticker':ticker,
                                     'DE Ratio':value,
-                                    'Price':stock_price}, ignore_index = True)
+                                    'Price':stock_price,
+                                    'stock_p_change':stock_p_change,
+                                    'SP500':sp500_value,
+                                    'sp500_p_change':sp500_p_change}, ignore_index = True)
                 except Exception as e:
                     pass
 
